5b15b1d3ee48e009a7bedb2f72d0843a2e776a81
Evan Kim <ekim2339@gmail.com>
2024-05-13 21:57:38 -0400

    icp transform

--- view_pointcloud.py
+++ view_pointcloud.py
@@ -3,18 +3,21 @@ import trimesh
 dat_points = np.load("/om/user/evan_kim/SculptFormer/datasets/data/shapenet/data_tf/02691156/98b163efbbdf20c898dc7d57268f30d4/rendering/03.dat", allow_pickle=True, encoding='bytes')
 dat_points = dat_points[:, :3] / 0.19
 
-new_order = [0, 2, 1]
+# new_order = [0, 2, 1]
 # new_order = [1, 0, 2]
 # new_order = [1, 2, 0]
 # new_order = [2, 0, 1]
 # new_order = [2, 1, 0]
 
-dat_points = dat_points[:, new_order]
 point_cloud_dat = trimesh.points.PointCloud(dat_points, colors=[255, 0, 0])
 
 mesh = trimesh.load("/om/user/evan_kim/InstantMesh/outputs/instant-mesh-large/meshes/02691156.obj", process=True)
 mesh_points, _ = trimesh.sample.sample_surface(mesh, 8179)
 point_cloud_mesh = trimesh.points.PointCloud(mesh_points, colors=[0, 255, 0])
+
+result = trimesh.registration.icp(point_cloud_dat, point_cloud_mesh)
+# Apply the transformation to align point_cloud_2
+point_cloud_mesh.apply_transform(result.transformation)
 
 # Visualize the point clouds
 scene = trimesh.Scene()
