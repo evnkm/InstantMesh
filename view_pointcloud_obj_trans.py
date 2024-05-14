9579ab37681d7487a5e8e3a5fc2bca05195d4c1d
Evan Kim <ekim2339@gmail.com>
2024-05-14 01:05:51 -0400

    transforming obj according to render metadata

--- view_pointcloud_obj_trans.py
+++ view_pointcloud_obj_trans.py
@@ -14,54 +14,49 @@ SCALING_FACTOR = 0.19
 # new_order = [2, 0, 1]
 # new_order = [2, 1, 0]
 
-def inverse_transform(train_data, param):
-    # Unpack camera parameters
-    theta = np.deg2rad(param[0])
-    phi = np.deg2rad(param[1])
-    camY = param[3] * np.sin(phi)
-    temp = param[3] * np.cos(phi)
-    camX = temp * np.cos(theta)
-    camZ = temp * np.sin(theta)
-    cam_pos = np.array([camX, camY, camZ])
-
-    # Compute camera matrix
-    axisZ = cam_pos.copy()
-    axisY = np.array([0, 1, 0])
-    axisX = np.cross(axisY, axisZ)
-    axisY = np.cross(axisZ, axisX)
-    cam_mat = np.array([unit(axisX), unit(axisY), unit(axisZ)])
-
-    # Extract transformed positions and normals
-    pt_trans = train_data[:, :3]
-    nom_trans = train_data[:, 3:]
-
-    # Inverse transformation for positions
-    position = np.dot(pt_trans, cam_mat) + cam_pos
-
-    # Inverse transformation for normals
-    # normal = np.dot(nom_trans, cam_mat)
-
-    return position #, normal
-
 def unit(v):
     norm = np.linalg.norm(v)
     if norm == 0:
         return v
     return v / norm
-render_meta = np.loadtxt(RENDERING_METADATA)
-dat_points = np.load(DAT_FILE, allow_pickle=True, encoding='bytes')
-param = render_meta[PHOTO_IDX]
 
-dat_points = inverse_transform(dat_points, param) / SCALING_FACTOR
+def camera_info(param):
+    theta = np.deg2rad(param[0])
+    phi = np.deg2rad(param[1])
+
+    camY = param[3]*np.sin(phi)
+    temp = param[3]*np.cos(phi)
+    camX = temp * np.cos(theta)    
+    camZ = temp * np.sin(theta)        
+    cam_pos = np.array([camX, camY, camZ])        
+
+    axisZ = cam_pos.copy()
+    axisY = np.array([0,1,0])
+    axisX = np.cross(axisY, axisZ)
+    axisY = np.cross(axisZ, axisX)
+
+    cam_mat = np.array([unit(axisX), unit(axisY), unit(axisZ)])
+    return cam_mat, cam_pos
+
+
+dat_points = np.load(DAT_FILE, allow_pickle=True, encoding='bytes')[:, :3]
 # shift the points to the origin
 dat_points -= np.mean(dat_points, axis=0)
 point_cloud_dat = trimesh.points.PointCloud(dat_points, colors=[255, 0, 0])
 
+# rendering details
+render_meta = np.loadtxt(RENDERING_METADATA)
+param = render_meta[PHOTO_IDX]
+cam_mat, cam_pos = camera_info(param)
+
 mesh = trimesh.load(OBJ_FILE, process=True)
 mesh_points, _ = trimesh.sample.sample_surface(mesh, 8179)
+mesh_points *= 0.57
+mesh_points_transformed = np.dot(mesh_points-cam_pos, cam_mat.transpose())
+
 # shift the points to the origin
-mesh_points -= np.mean(mesh_points, axis=0)
-point_cloud_mesh = trimesh.points.PointCloud(mesh_points, colors=[0, 255, 0])
+mesh_points_transformed -= np.mean(mesh_points_transformed, axis=0)
+point_cloud_mesh = trimesh.points.PointCloud(mesh_points_transformed, colors=[0, 255, 0])
 
 # Visualize the point clouds
 scene = trimesh.Scene()
